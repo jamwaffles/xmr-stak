509bfb62782216c7cee09c2ea2518152bb48dad3
James Waples <james@cambridge-intelligence.com>
2017-11-29 13:47:30 +0000

    Add start script

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+./bin/xmr-stak \
+  --currency monero \
+  -o stratum+tcp://xmr-eu1.nanopool.org:14444 \
+  -u 47cesuh5C8MbTyqH95KPotDuhrp5gsTx7Fqt5iUuUuTjDWFDF9VfDtMF51v2aJqwPq54eDeEP6uZAbN8wU2bqoAs2BLLfEM.0.mbp/jamwaffles@gmail.com \
+  -p x
